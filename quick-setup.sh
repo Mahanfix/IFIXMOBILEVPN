be6fea199aef29509406c437f765ce69f2a78204
ifixripper <121984414+Mahanfix@users.noreply.github.com>
2026-08-21 04:23:44 +0330

    Add quick setup script - one-liner installation

---
+++ quick-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# استفاده: curl -fsSL https://raw.githubusercontent.com/Mahanfix/IFIXMOBILEVPN/main/quick-setup.sh | sudo bash
+
+wget -q https://raw.githubusercontent.com/Mahanfix/IFIXMOBILEVPN/main/install.sh -O /tmp/install.sh && sudo bash /tmp/install.sh
